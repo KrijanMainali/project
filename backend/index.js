1c3ddaf399507503bafd91073846e761e87ce417
KrijanMainali <krijanmainali99@gmail.com>
2026-01-01 23:04:35 +0545

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.listen
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+        console.log(`server is running at http://localhost:${PORT}`);
+});
+
+
+app.get('/',(req,res)=>{
+
+    res.send("<h1>hello world</h1>");
+
+});
